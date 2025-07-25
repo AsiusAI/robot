cc4b7476267a0f363ccbd8a9ff4abce77af65f98
Karel Nagel <nagelkarel@gmail.com>
2025-07-25 17:11:49 +0300

    neutralize

--- neutralize.py
+++ neutralize.py
@@ -1,22 +1,22 @@
+import argparse
 from servos import STS3215, SCS0009
 from servo import PortHandler, ServoConnection
 import time
 
+parser = argparse.ArgumentParser(description="Neutralize servo to 512")
+parser.add_argument("id", type=int, nargs="?", default=1, help="Current servo ID")
+args = parser.parse_args()
+
 servo = SCS0009()
-conn = ServoConnection("/dev/tty.usbmodem5A7A0572801",servo.BIG_ENDIAN)
+conn = ServoConnection("/dev/tty.usbmodem5A7A0572801", servo.BIG_ENDIAN)
 
-id = 1
-pos, _, _ = conn.read(id, servo.PRESENT_POSITION)
-curr_min, _, _ = conn.read(id, servo.MIN_POSITION_LIMIT)
-curr_max, _, _ = conn.read(id, servo.MAX_POSITION_LIMIT)
-print(f"{curr_min=} {curr_max=} {pos=}")
+pos, _, _ = conn.read(args.id, servo.PRESENT_POSITION)
+print(f"Current position {pos}")
 
-conn.write(id, servo.TORQUE_ENABLE, 1)
-
-conn.write(id, servo.GOAL_POSITION, 512)
+conn.write(args.id, servo.TORQUE_ENABLE, 1)
+conn.write(args.id, servo.GOAL_POSITION, 512)
 time.sleep(2)
+conn.write(args.id, servo.TORQUE_ENABLE, 0)
 
-conn.write(id, servo.TORQUE_ENABLE, 0)
-pos, _, _ = conn.read(id, servo.PRESENT_POSITION)
-print(f"{pos=}")
-
+pos, _, _ = conn.read(args.id, servo.PRESENT_POSITION)
+print(f"Moved to {pos}")
