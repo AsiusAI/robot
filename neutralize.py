dddadc96bdf6f3fd5a9e4a6aa0514c0d167cad35
Karel Nagel <nagelkarel@gmail.com>
2025-07-25 16:30:01 +0300

    neutralize

---
+++ neutralize.py
@@ -0,0 +1,34 @@
+from servos import STS3215, SCS0009
+from servo import PortHandler, Connection
+import time
+
+servo = SCS0009()
+port = PortHandler("/dev/tty.usbmodem5A7A0572801")
+conn = Connection(servo.BIG_ENDIAN, port)
+
+if not port.openPort():
+    print("Failed to open the port")
+    exit()
+
+if not port.setBaudRate(1_000_000):
+    print("Setting baud rate failed")
+    exit()
+
+try:
+    id = 1
+    pos, _, _ = conn.read(id, servo.PRESENT_POSITION)
+    curr_min, _, _ = conn.read(id, servo.MIN_POSITION_LIMIT)
+    curr_max, _, _ = conn.read(id, servo.MAX_POSITION_LIMIT)
+    print(f"{curr_min=} {curr_max=} {pos=}")
+
+    conn.write(id, servo.TORQUE_ENABLE, 1)
+
+    conn.write(id, servo.GOAL_POSITION, 512)
+    time.sleep(2)
+
+    conn.write(id, servo.TORQUE_ENABLE, 0)
+    pos, _, _ = conn.read(id, servo.PRESENT_POSITION)
+    print(f"{pos=}")
+
+finally:
+    port.closePort()
