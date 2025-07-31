bd801e9e27c2eefa7883775594b8001888ca351d
Karel Nagel <nagelkarel@gmail.com>
2025-07-31 21:13:44 +0300

    save

--- move.py
+++ move.py
@@ -1,32 +1,30 @@
 import argparse
 import os
-from arms import Arm
+import time
+from arms import Arm, Hand
 from servo import Port
 
 parser = argparse.ArgumentParser(description='Move')
-parser.add_argument('port', type=str, nargs='?', default=os.getenv('PORT'), help='Port')
+parser.add_argument('--port', type=str, default=os.getenv('PORT'), help='Port')
 args = parser.parse_args()
 
 port = Port(args.port)
 
-right = Arm('right', port)
+right = Hand('right', port)
+servos = right.servos
 
-# servo = right.hand_pitch
 
-# servo.conn.write(servo.id, servo.conn.LOCK, 0)
-# print(servo.conn.write(servo.id, servo.conn.OPERATING_MODE, 0))
-# print(servo.conn.write(servo.id, servo.conn.PHASE, 28))
-# print(servo.conn.write(servo.id, servo.conn.MIN_POSITION_LIMIT, 0))
-# print(servo.conn.write(servo.id, servo.conn.MAX_POSITION_LIMIT, 25000))
-# print(servo.conn.write(servo.id, servo.conn.TORQUE_ENABLE, 128))
-# servo.conn.write(servo.id, servo.conn.LOCK, 1)
-# # print(servo)
+for servo in servos:
+  servo.start()
 
-# servo.start()
-# print(servo.conn.read(servo.id, servo.conn.PRESENT_POSITION))
-# print(servo.conn.write(servo.id, servo.conn.GOAL_POSITION, 32767))
-# time.sleep(4)
-# servo.stop()
+for _ in range(3):
+  for servo in servos:
+    servo.move(1)
 
-for servo in right.servos:
-  print(servo.conn.read(servo.id, servo.conn.HOMING_OFFSET))
+  time.sleep(2)
+  
+  for servo in servos:servo.move(0)
+  time.sleep(2)
+
+for servo in servos:
+  servo.stop()
